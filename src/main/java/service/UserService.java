8d69984458098140f95a7b81d3635624e6f487a1
jack seymour <stilljack@gmail.com>
2019-11-08 10:46:48 -0800

    this is pretty show stopping for testing which makes writing any more code either an exercise in imagination or philosophy... speaking of philosophy, god i miss that subject...
    We're take five and breathe. see ya space cowboy.

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface UserService {
  List<User> findAll();
  User findUserById(long id);
+
  User update(User user, long id);
  void delete(long id);
  User save (User user);
